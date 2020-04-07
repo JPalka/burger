4b5aea9ea5841d16035531ed5541899eb4e86cf1
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-04-07 22:06:43 +0200

    Add removing ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,15 +32,32 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
-
+    if (this.state.ingredients[type] <= 0) {
+      return;
+    }
+    const newCount = this.state.ingredients[type] - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = newCount;
+    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return(
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControl 
-          ingredientAdded={this.addIngredientHandler} />
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler} 
+          disabled={disabledInfo}/>
       </Aux>
     )
   }
