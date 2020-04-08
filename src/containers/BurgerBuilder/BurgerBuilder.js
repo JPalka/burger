4474a04929a9a5cfcceb0d832f96ceb450658518
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-04-08 12:04:05 +0200

    Show and hide modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   }
 
   updatePurchaseState = (ingredients) => {
@@ -60,6 +61,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -69,7 +74,7 @@ class BurgerBuilder extends Component {
     }
     return(
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -78,6 +83,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler} 
           disabled={disabledInfo}
           price={this.state.totalPrice}
+          ordered={this.purchaseHandler}
           purchasable={this.state.purchasable}/>
       </Aux>
     )
