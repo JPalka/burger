2006fe5cd0666138329ea28a7507e0a21b36d5f2
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-04-20 12:06:27 +0200

    Add query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -105,7 +105,14 @@ class BurgerBuilder extends Component {
     //     this.setState({ loading: false, purchasing: false });
     //     console.log(error);
     //   });
-    this.props.history.push('/checkout');
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryParams.join('&')
+    });
   }
 
   render() {
