9cf047dde899248dc8cb758c44edb08cfd63c12c
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-05-01 10:16:42 +0200

    Add order display

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,26 @@ import React from 'react';
 import classes from './Order.module.css';
 
 const order = (props) => {
+  let ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push(
+      {
+        ingredient: ingredientName, amount: props.ingredients[ingredientName]
+      }
+    );
+  }
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span 
+        style={{textTransform: 'capitalize', display: 'inline-block', margin: '0 8px', border: '1px solid #ccc', padding: '5px'}}
+        key={ig.ingredient}>{ig.ingredient} ({ig.amount})
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>USD 5.22</strong></p>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
   )
 }
